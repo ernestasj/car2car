15841e67678b2a5100d09e73ad472160f5c8819c
Monte <JMonte@live.com.au>
2018-04-24 18:10:26 +1000

    Frontend Amendments addCard program

--- Frontend/php/config.php
+++ Frontend/php/config.php
@@ -1,7 +1,7 @@
 <?php
    define('DB_SERVER', 'localhost:3306');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'pizzacat');
+   define('DB_USERNAME', 'daddy');
+   define('DB_PASSWORD', 'yeet');
    define('DB_DATABASE', 'car');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE) or die("Error " . mysqli_error($rs));
 ?>
