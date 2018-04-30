c2e23a77d79f9d7e3bf66f7b47d2a34ebbf55d92
Michael O'Ryan <michael.oryan@gmail.com>
2018-04-30 19:50:51 +1000

    login refactored to _ template

--- Frontend/js/login.js
+++ Frontend/js/login.js
@@ -1,12 +1,62 @@
 var Login = {};
 
+Login.Header = Template.Header({
+    title: "Welcome"
+});
+/*
 Login.Header = '\
     <div class="col-lg-12">\
         <h1 class="page-header">Welcome</h1>\
     </div>\
     <!-- /.col-lg-12 -->';
+*/
+Login.Body = {};
+Login.Form = {};
 
-    Login.Body = '\
+Login.Form.Email = Template.InputGroup({
+    label: "Email",
+    name: "email",
+    type: "text",
+    placeholder: "email address",
+    classes: "",
+    id: "email"
+});
+
+Login.Form.Password = Template.InputGroup({
+    label: "Password",
+    name: "password",
+    type: "password",
+    placeholder: "password",
+    classes: "",
+    id: "password"
+});
+
+Login.Form.Login = Template.Submit({
+    label:"Login",
+    id: "btnLogin"
+});
+
+Login.Form.CreateAccount = Template.Submit({
+    label:"Create Account",
+    id: "btnCreate"
+});
+
+Login.Body.Form = Template.Form({
+    formid: "formlogin",
+    method: "post",
+    groups: 
+    Login.Form.Email +
+    Login.Form.Password +
+    Login.Form.Login +
+    Login.Form.CreateAccount
+});
+
+Login.Body.PanelBody = Template.PanelBody({
+    title:"Login",
+    body: Login.Body.Form
+});
+/*
+Login.Body = '\
     <div class="col-lg-12" >\
         <div class="list-car-panel panel panel-default">\
             <div class="panel-heading">\
@@ -31,10 +81,10 @@ Login.Header = '\
             </div>\
         </div>\
     </div>';
-
+*/
 Login.Display = function(page){
     $("#"+page.header).html(Login.Header);
-    $("#"+page.body).html(Login.Body);
+    $("#"+page.body).html(Login.Body.PanelBody);
 
     Util.PrepareForm("#formlogin", "../submit/login.php", "#btnLogin", "", Login.Check);
     Util.ButtonListener("#btnCreate", CreateAccount.Display, Page.Layout);
