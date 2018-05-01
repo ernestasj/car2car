e50a43d25c00479cd654b778cfa3ce7838fb6c17
Conqabonkadonk <37131361+Conqabonkadonk@users.noreply.github.com>
2018-05-01 20:54:53 +1000

    Created Account Class

---
+++ Frontend/php/classes/accounts.php
@@ -0,0 +1,40 @@
+<?php
+class Account {
+    public $email;
+    public $password;
+    public $firstname;
+    public $lastname;
+    public $licence;
+    public $cardtype;
+    public $ccnumber;
+    public $expiry;
+    public $cvc;
+    public $cardname;
+
+    function __construct($email, $password, $firstname,
+                         $lastname, $licence, $cardtype, 
+                         $ccnumber, $expiry, $cvc, $cardname) 
+    {
+        $this->email = $email;
+        $this->password = $password;
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+        $this->licence = $licence;
+        $this->cardtype = $cardtype;
+        $this->ccnumber = $ccnumber;
+        $this->expiry = $expiry;
+        $this->cvc = $cvc;
+        $this->cardname = $cardname;
+    }
+
+    function WriteDB($db)
+    {
+        //Converts this class to json
+        $jsonTemp = json_encode($this);
+        //Writes to database
+            //Needs to go here
+    }
+    
+
+}
+?>
