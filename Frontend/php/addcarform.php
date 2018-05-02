93ae5f7a643d4f4da15668e47b6dfb418b9f1b1b
Conqabonkadonk <37131361+Conqabonkadonk@users.noreply.github.com>
2018-05-02 12:44:35 +1000

    changes to car form
    changes to car form

--- Frontend/php/addcarform.php
+++ Frontend/php/addcarform.php
@@ -12,28 +12,9 @@
 'transmission' - transmission type - alpha
 */
 
-class CarFormClass
-{
-    public $make;
-    public $doors;
-    public $petrol;
-    public $body;
-    public $transmission;
+//Includes Class for storing the returned data
+include("carform.php");
 
-    function __constructor()
-    {
-        $this->make = array();
-        $this->door = array();
-        $this->petrol = array();
-        $this->body = array();
-        $this->transmission = array();
-    }
 
-    function refreshValues()
-    {
-        //Calls to database to get values for fields
-    }
-
-}
 
 ?>
---
+++ Frontend/php/classes/carform.php
@@ -0,0 +1,26 @@
+<?php
+class CarFormClass
+{
+    public $make;
+    public $doors;
+    public $petrol;
+    public $body;
+    public $transmission;
+
+    function __constructor()
+    {
+        $this->make = array();
+        $this->door = array();
+        $this->petrol = array();
+        $this->body = array();
+        $this->transmission = array();
+        $this->refreshValues();
+    }
+
+    function refreshValues()
+    {
+        //Calls to database to get values for fields
+    }
+
+}
+?>
