9fdba94ce771ffb84b9d2244d3786670c32ca34d
Michael O'Ryan <michael.oryan@gmail.com>
2018-05-05 10:36:57 +1000

    Search and results + angular navbar and results list

---
+++ Frontend/navbar.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+    $("#nav_login").click(function(){
+          window.location.href = "./login.html";
+    });
+    $("#nav_signup").click(function(){
+          window.location.href = "./createaccount.html";
+    });
+/*
+    $("#nav_search").click(function(){
+        window.location.href = "./results.html";
+    });
+*/
+});
