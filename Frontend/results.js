03e51c99e8f9b48563f2ee59e658f636bad81041
Michael O'Ryan <michael.oryan@gmail.com>
2018-05-07 17:25:34 +1000

    Feedback on booking from results modal

--- Frontend/results.js
+++ Frontend/results.js
@@ -18,6 +18,10 @@ function GetURLParameter(sParam)
 
 app.controller('resultsCtrl', function($scope, $http) {
     $scope.yes = "yes";
+    $scope.requested = false;
+    
+    $scope.booking_class = "btn-primary";
+    $scope.action_text = "Book";   
     /*
         var cars = [
             {
@@ -64,11 +68,54 @@ app.controller('resultsCtrl', function($scope, $http) {
         $scope.pizza = "Yum!";
     //$http.get("customers.php").then(function (response) {
         */
+    $scope.book = function(car) {
+        console.log("Booking!");
+        $http.post("./submit/booking.php",
+        {
+            rego: car.rego,
+            date: car.date
+        })
+        .then(function(response) {
+            console.log(response)
+            $scope.requested = true;
+            //<button type="button" class="btn btn-danger col-sm-2" ng-show="requested" ng-click="unbook(car)">Cancel Booking</button>
+
+            $scope.action = $scope.unbook;
+            $scope.booking_class = "btn-danger";
+            $scope.action_text = "Cancel";  
+        });
+
+
+    }
+
+    $scope.unbook = function(car) {
+        console.log("Unbooking!");
+        
+        $http.post("./submit/unbooking.php",
+        {
+            rego: car.rego,
+            date: car.date
+        })
+        .then(function(response) {
+            console.log(response)
+            $scope.requested = false;  
+            $scope.action = $scope.book;
+            $scope.booking_class = "btn-primary";
+            $scope.action_text = "Book";   
+
+        });
+
+
+    }
+
+
         $scope.show_modal = function(car){
             console.log(car);
             $scope.car = car;
+            $scope.car.date = "";
             $("#detailed").modal("show");
         };
+        
         $http.post("./json/search.php",
         {
             keywords: GetURLParameter("keywords")
@@ -78,7 +125,7 @@ app.controller('resultsCtrl', function($scope, $http) {
             $scope.cars = response.data;
         });
         
-        
+        $scope.action = $scope.book;
     //});
 });
 
