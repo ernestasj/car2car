0a2ca1140e32318c58d220e4273499d21a1d01ef
Michael O'Ryan <michael.oryan@gmail.com>
2018-05-09 18:05:39 +1000

    DB schema for updatedetails

--- database_server.sql
+++ database_server.sql
@@ -1125,6 +1125,44 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `UpdateAccount` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `UpdateAccount`(email VARCHAR(45), password VARCHAR(45), firstname VARCHAR(45), lastname VARCHAR(45), licence VARCHAR(45), dob VARCHAR(45), number VARCHAR(45), street VARCHAR(45), suburb VARCHAR(45), state VARCHAR(45), postcode VARCHAR(45), mobnumber VARCHAR(45), landline VARCHAR(45), ccnumber VARCHAR(45), cvc VARCHAR(45), cardtype VARCHAR(45), expiry  VARCHAR(45))
+BEGIN
+	UPDATE user SET
+#		email = email,
+        password = password,
+        firstname = firstname,
+        lastname = lastname,
+        licence = licence,
+        dob = dob,
+        number = number,
+        street = street,
+        suburb = suburb,
+        state = state,
+        postcode = postcode,
+        mobnumber = mobnumber,
+        landline = landline,
+        ccnumber = ccnumber,
+        cvc = cvc,
+        cardtype = cardtype,
+        expiry = expiry
+        WHERE
+        email = email;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `UpdateRate` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -1173,4 +1211,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-05-09 17:02:18
+-- Dump completed on 2018-05-09 18:05:15
