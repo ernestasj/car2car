4d393519eb1a5992d2928f66d86fa307fbdf7584
Conqabonkadonk <37131361+Conqabonkadonk@users.noreply.github.com>
2018-05-17 00:28:46 +1000

    Finalize Bank

--- Frontend/bank/transactionHandler.php
+++ Frontend/bank/transactionHandler.php
@@ -53,6 +53,30 @@ function deductFromAccount($card, $cvv, $amount, $con)
         return 1;
     }
 }
+function addToAccount($card, $cvv, $amount, $con)
+{//ERRORS: 0 = NO ERROR | 1 = INCORRECT ACCOUNT DETAILS
+    if (checkCVV($card, $cvv, $con) == true)
+    {
+        $getAccountQuery = "SELECT * FROM Account WHERE accountId = (SELECT accountId FROM card WHERE cardNum = '".$card."')";
+        $result = mysqli_query($con, $getAccountQuery);
+        if (mysqli_num_rows($result) > 0)
+        {
+            $row = mysqli_fetch_assoc($result);
+            $newBalance = $row['balance'] + $amount;
+            $updateStatement = "UPDATE ACCOUNT SET balance = '".$newBalance."' WHERE accountId = (SELECT accountId FROM card WHERE cardNum = '".$card."')";
+            mysqli_query($con, $updateStatement);
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+    else
+    {
+        return 1;
+    }
+}
 
 
 function testFunc($con)
