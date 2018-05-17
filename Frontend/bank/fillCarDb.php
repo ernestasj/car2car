c4e10a108326f3d723b883e1bb6c0946b65312bd
Conqabonkadonk <37131361+Conqabonkadonk@users.noreply.github.com>
2018-05-17 13:22:09 +1000

    CarDb Filler

--- Frontend/bank/fillCarDb.php
+++ Frontend/bank/fillCarDb.php
@@ -1,17 +1,144 @@
 <?php   
+class ModelClass
+{
+    public $make;
+    public $model;
+    function __construct($make, $model)
+    {
+        $this->make = $make;
+        $this->model = $model;
+    }
+}
+$numberOfEntries = 100;
+$globalDoors = 4;
+$globalCC = 1000;
+$yearMin = 1995;
+$yearMax = 2018;
+$kmsMax = 500000;
+$globalBody = "body";
+$modelArray = array();
+$regoArray = array();
+$globalPetrol = "petrol";
+$transmission = array("auto", "manual");
+
+
+
+//Connecting To Database
+$servername = "localhost:3306";
+$dbusername = "root";
+$dbpassword = "";
+//Creating Connection
+
+$con = mysqli_connect($servername, $dbusername, $dbpassword, 'test');
+
+//Checking Connection
+if (!$con)
+{
+	die("Connection Failed: " . mysqli_connect_error());
+}
+echo "CONNECTED!<br>";
+// Connected successfully
+//Getting Data
+mysqli_query($con, "use car");
+$getCarDetails = "SELECT * FROM model;";
+$modelResult = mysqli_query($con, $getCarDetails);
+if (mysqli_num_rows($modelResult) > 0)
+{
+	while ($row = mysqli_fetch_assoc($modelResult))
+	{
+        $modelArray[count($modelArray)] = new ModelClass($row["make"], $row["name"]);
+    }
+}
+
+
+
+foreach ($modelArray as $value)
+{
+    echo " - ".$value->make." | ".$value->model."<br>";
+}
+
+
+
+
+$count = 0;
+$alphabet = array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+$regoNum1 = $regoNum2 = $regoNum3 = $regoNum4 = 0;
+$regoNumMain = 10;
+while ($count <= $numberOfEntries)
+{
+    $returnVal = "".$alphabet[$regoNum1].$alphabet[$regoNum2].$regoNumMain.$alphabet[$regoNum3].$alphabet[$regoNum4];
+    $regoNum1++;
+    if ($regoNum1 > 25)
+    {
+        $regoNum1 = 0;
+        $regoNum2++;
+        if ($regoNum2 > 25)
+        {
+            $regoNum2 = 0;
+            $regoNum3++;
+            if ($regoNum3 > 25)
+            {
+                $regoNum3 = 0;
+                $regoNum4++;
+                if ($regoNum4 > 25)
+                {
+                    $regoNum4 = 0;
+                }
+            }
+        }
+    }
+
+    $regoNumMain = $regoNumMain + rand(1, 10);
+    if ($regoNumMain > 99)
+    {
+        $regoNumMain = 10;
+    }
+    echo $count.") ".$returnVal."<br>";
+    $regoArray[$count] = $returnVal;
+    $count++;
+}
+
+
 /*==================================
-    rego varchar(45) NOT NULL,
-    make varchar(45),
-    model varchar(45),
-    manufacturer varchar(45),
-    petrol varchar(45),
-    transmission varchar(45),
-    year varchar(45),
-    doors varchar(45),
-    enginecc varchar(45),
-    kms varchar(45),
-    body varchar(45),
+$numberOfEntries = 100;
+$globalDoors = 4;
+$globalCC = 1000;
+$yearMin = 1995;
+$yearMax = 2018;
+$kmsMax = 500000;
+$body = "body";
+$modelArray = array();
+$regoArray = array();
+$globalPetrol = "petrol";
 ==================================*/
+
+//====================================
+//Filling Database
+mysqli_query($con, "use fakeRMS");
+
+$count = 0;
+while ($count < $numberOfEntries)
+{
+    $tranNum = rand(0, 1);
+    $kms = rand(0, $kmsMax);
+    $yearTemp = rand($yearMin, $yearMax);
+    $makeNum = rand(0, count($modelArray)-1);
+    $insertStatement = "INSERT INTO carlist VALUES(
+        '".$regoArray[$count]."',
+        '".$modelArray[$makeNum]->make."',
+        '".$modelArray[$makeNum]->model."',
+        '".$modelArray[$makeNum]->make."',
+        '".$globalPetrol."',
+        '".$transmission[$tranNum]."',
+        '".$yearTemp."',
+        '".$globalDoors."',
+        '".$globalCC."',
+        '".$kms."', 
+        '".$globalBody."'
+        )";
+    mysqli_query($con, $insertStatement);
+    $count++;
+}
 
 
 
