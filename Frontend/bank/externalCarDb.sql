cb4b265985e3ac37bd00c33acf56edc8bba47e81
Conqabonkadonk <37131361+Conqabonkadonk@users.noreply.github.com>
2018-05-17 12:34:04 +1000

    Adding Car DB Stuff

---
+++ Frontend/bank/externalCarDb.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE  IF NOT EXISTS `FakeRMS`;
+USE `FakeRMS`;
+
+
+DROP TABLE IF EXISTS CarList;
+
+CREATE TABLE CarList
+(
+    rego varchar(45) NOT NULL,
+    make varchar(45),
+    model varchar(45),
+    manufacturer varchar(45),
+    petrol varchar(45),
+    transmission varchar(45),
+    year varchar(45),
+    doors varchar(45),
+    enginecc varchar(45),
+    kms varchar(45),
+    body varchar(45),
+    PRIMARY KEY (rego)
+);
+
---
+++ Frontend/bank/fillCarDb.php
@@ -0,0 +1,18 @@
+<?php   
+/*==================================
+    rego varchar(45) NOT NULL,
+    make varchar(45),
+    model varchar(45),
+    manufacturer varchar(45),
+    petrol varchar(45),
+    transmission varchar(45),
+    year varchar(45),
+    doors varchar(45),
+    enginecc varchar(45),
+    kms varchar(45),
+    body varchar(45),
+==================================*/
+
+
+
+?>
