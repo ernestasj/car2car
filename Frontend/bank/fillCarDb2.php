9a3aa2be2e218b4e869ce48755d17787e2f98843
Michael O'Ryan <michael.oryan@gmail.com>
2018-05-21 08:13:25 +1000

    Refactored generating cars for the car database. db needs to be filled.

--- Frontend/bank/fillCarDb2.php
+++ Frontend/bank/fillCarDb2.php
@@ -1,144 +1,100 @@
-<?php   //THIS IS FOR CAR DATABSE NOT FAKE RMS
-class ModelClass
-{
-    public $make;
-    public $model;
-    function __construct($make, $model)
+<?php   //Generate Car entries for the car database
+    include("../php/includes.php");
+
+    $numberOfEntries = 100;
+    $yearMin = 1995;
+    $yearMax = 2018;
+
+    $count = 0;
+    $alphabet = array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+    $regoNum1 = $regoNum2 = $regoNum3 = $regoNum4 = 0;
+    $regoNumMain = 10;
+    while ($count <= $numberOfEntries)
     {
-        $this->make = $make;
-        $this->model = $model;
-    }
-}
-$numberOfEntries = 100;
-$globalDoors = 4;
-$globalCC = 1000;
-$yearMin = 1995;
-$yearMax = 2018;
-$kmsMax = 500000;
-$globalBody = "body";
-$modelArray = array();
-$regoArray = array();
-$globalPetrol = "petrol";
-$transmission = array("auto", "manual");
-
-
-
-//Connecting To Database
-$servername = "localhost:3306";
-$dbusername = "root";
-$dbpassword = "";
-//Creating Connection
-
-$con = mysqli_connect($servername, $dbusername, $dbpassword, 'test');
-
-//Checking Connection
-if (!$con)
-{
-	die("Connection Failed: " . mysqli_connect_error());
-}
-echo "CONNECTED!<br>";
-// Connected successfully
-//Getting Data
-mysqli_query($con, "use car");
-$getCarDetails = "SELECT * FROM model;";
-$modelResult = mysqli_query($con, $getCarDetails);
-if (mysqli_num_rows($modelResult) > 0)
-{
-	while ($row = mysqli_fetch_assoc($modelResult))
-	{
-        $modelArray[count($modelArray)] = new ModelClass($row["make"], $row["name"]);
-    }
-}
-
-
-
-foreach ($modelArray as $value)
-{
-    echo " - ".$value->make." | ".$value->model."<br>";
-}
-
-
-
-
-$count = 0;
-$alphabet = array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
-$regoNum1 = $regoNum2 = $regoNum3 = $regoNum4 = 0;
-$regoNumMain = 10;
-while ($count <= $numberOfEntries)
-{
-    $returnVal = "".$alphabet[$regoNum1].$alphabet[$regoNum2].$regoNumMain.$alphabet[$regoNum3].$alphabet[$regoNum4];
-    $regoNum1++;
-    if ($regoNum1 > 25)
-    {
-        $regoNum1 = 0;
-        $regoNum2++;
-        if ($regoNum2 > 25)
+        $returnVal = "".$alphabet[$regoNum1].$alphabet[$regoNum2].$regoNumMain.$alphabet[$regoNum3].$alphabet[$regoNum4];
+        $regoNum1++;
+        if ($regoNum1 > 25)
         {
-            $regoNum2 = 0;
-            $regoNum3++;
-            if ($regoNum3 > 25)
+            $regoNum1 = 0;
+            $regoNum2++;
+            if ($regoNum2 > 25)
             {
-                $regoNum3 = 0;
-                $regoNum4++;
-                if ($regoNum4 > 25)
+                $regoNum2 = 0;
+                $regoNum3++;
+                if ($regoNum3 > 25)
                 {
-                    $regoNum4 = 0;
+                    $regoNum3 = 0;
+                    $regoNum4++;
+                    if ($regoNum4 > 25)
+                    {
+                        $regoNum4 = 0;
+                    }
                 }
             }
         }
+        $regoNumMain = $regoNumMain + 1;
+        if ($regoNumMain > 99)
+        {
+            $regoNumMain = 10;
+        }
+        echo $count.") ".$returnVal."<br>";
+        $regoArray[$count] = $returnVal;
+        $count++;
     }
-    $regoNumMain = $regoNumMain + 1;
-    if ($regoNumMain > 99)
+
+
+    /*==================================
+    $numberOfEntries = 100;
+    $globalDoors = 4;
+    $globalCC = 1000;
+    $yearMin = 1995;
+    $yearMax = 2018;
+    $kmsMax = 500000;
+    $body = "body";
+    $modelArray = array();
+    $regoArray = array();
+    $globalPetrol = "petrol";
+    ==================================*/
+
+    //====================================
+    //Filling Database
+
+    $count = 0;
+    while ($count < $numberOfEntries)
     {
-        $regoNumMain = 10;
+        $yearTemp = rand($yearMin, $yearMax);
+        $suburb = RandomSuburb($db);
+        $user = RandomUser($db);
+        $makeModel = RandomMakeModel($db);
+
+        $desc["rego"] = $regoArray[$count];
+        $desc["make"] = $makeModel["make"];
+        $desc["model"] = $makeModel["model"];
+        $desc["year"] = $yearTemp;
+        $desc["doors"] = RandomDoors($db);
+        $desc["petrol"] = RandomPetrol($db);
+        $desc["transmission"] = RandomTransmission($db);
+        $desc["enginecc"] = RandomEnginecc($db);
+        $desc["kms"] = RandomKMS($db);
+        $desc["body"] = RandomPetrol($db);
+        $desc["monday"] = array_rand(["yes"], ["no"]);
+        $desc["tuesday"] = array_rand(["yes"], ["no"]);
+        $desc["wednesday"] = array_rand(["yes"], ["no"]);
+        $desc["thursday"] = array_rand(["yes"], ["no"]);
+        $desc["friday"] = array_rand(["yes"], ["no"]);
+        $desc["saturday"] = array_rand(["yes"], ["no"]);
+        $desc["sunday"] = array_rand(["yes"], ["no"]);
+        $desc["public_holidays"] = array_rand(["yes"], ["no"]);
+        $desc["postcode"] = $suburb["postcode"];
+        $desc["suburb"] = $suburb["name"];
+        $desc["state"] = $suburb["state"];
+        $desc["description"] = RandomDescription($db);
+        $desc["rate"] = rand(15, 100);
+
+        $car = new Car($desc);
+        $car->UpdateDB($db, $user["email"]);
+        $count++;
     }
-    echo $count.") ".$returnVal."<br>";
-    $regoArray[$count] = $returnVal;
-    $count++;
-}
-
-
-/*==================================
-$numberOfEntries = 100;
-$globalDoors = 4;
-$globalCC = 1000;
-$yearMin = 1995;
-$yearMax = 2018;
-$kmsMax = 500000;
-$body = "body";
-$modelArray = array();
-$regoArray = array();
-$globalPetrol = "petrol";
-==================================*/
-
-//====================================
-//Filling Database
-
-$count = 0;
-while ($count < $numberOfEntries)
-{
-    $tranNum = rand(0, 1);
-    $kms = rand(0, $kmsMax);
-    $yearTemp = rand($yearMin, $yearMax);
-    $makeNum = rand(0, count($modelArray)-1);
-    $insertStatement = "INSERT INTO car VALUES(
-        '".$regoArray[$count]."',
-        '".$modelArray[$makeNum]->make."',
-        '".$modelArray[$makeNum]->model."',
-        '".$modelArray[$makeNum]->make."',
-        '".$globalPetrol."',
-        '".$transmission[$tranNum]."',
-        '".$yearTemp."',
-        '".$globalDoors."',
-        '".$globalCC."',
-        '".$kms."', 
-        '".$globalBody."',
-        'default.jpg',
-        'bob'
-        )";
-    mysqli_query($con, $insertStatement);
-    $count++;
-}
 
 
 
--- Frontend/php/util.php
+++ Frontend/php/util.php
@@ -25,4 +25,57 @@
         $exts = $exts[$n];
         return $exts;
     } 
+
+    function RandomEntry($db, $functionName)
+    {
+        $stmt = $db->stmt_init();
+        $stmt = $db->prepare("call ".$functionName."()");
+        $stmt->bind_param("");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $entry;
+        if(mysqli_num_rows($result) > 0)
+        {
+            $row = mysqli_fetch_assoc($result);
+            $entry = $row;
+            
+        }
+        $stmt->close();
+        return $entry;        
+    }
+
+    function RandomSuburb($db) {
+        return RandomEntry($db, "RandomSuburb");
+    }
+
+    function RandomUser($db) {
+        return RandomEntry($db, "RandomUser");
+    }
+
+    function RandomMakeModel($db) {
+        return RandomEntry($db, "RandomMakeModel");
+    }
+
+    function RandomDoors($db) {
+        return RandomEntry($db, "RandomDoors");
+    }
+    function RandomTransmission($db) {
+        return RandomEntry($db, "RandomTransmission");
+    }
+
+    function RandomEnginecc($db) {
+        return RandomEntry($db, "RandomEnginecc");
+    }
+    function RandomBody($db) {
+        return RandomEntry($db, "RandomBody");
+    }
+
+    function RandomKMS($db) {
+        return RandomEntry($db, "RandomKMS");
+    }
+    
+    function RandomPetrol($db) {
+        return RandomEntry($db, "RandomPetrol");
+    }
+
 ?>
