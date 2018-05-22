44e1596b1ab712e9b9d5b1cde171031cb9fac0f7
Michael O'Ryan <michael.oryan@gmail.com>
2018-05-22 10:20:35 +1000

    Final Submission

---
+++ Frontend/php/config.php
@@ -0,0 +1,13 @@
+<?php
+    session_start();
+   define('DB_SERVER', 'localhost:3306');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', 'pizzacat');
+   define('DB_DATABASE', 'car');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE) or die("Error " . mysqli_error($rs));
+   $user;
+   if(isset($_SESSION['user']))
+   {
+       $user = unserialize($_SESSION['user']);
+   }
+?>
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # car2car
-CSIT314 Group Project 
+CSIT314 Group Project
+
+
+Use Mysql for the database.
+
+Use database_car.sql for a populated database. Simply run the sql file in mysql. Ass passwords are password for testing simplicity.
+
+Use Xampp for the server and point to the Frontend directory as the base http directory.
+
+Entry point is index.html which should be the default for a http server.
